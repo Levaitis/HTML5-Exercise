c968664651b997a06dafc470b743afdc44b371a6
Leonardo Vasi <leonardo@vasi.de>
2023-01-06 17:42:31 +0100

    Restructure: Divided javascript for accelerometer into separate files for specific use cases.

---
+++ js/accelerometer-graph.js
@@ -0,0 +1,42 @@
+
+let accelerometer = null;
+var error_card = document.getElementById("error_acc");
+var error_message = document.getElementById("error_acc_msg");
+error_card.style.display = "none";
+try {
+    accelerometer = new Accelerometer({ referenceFrame: 'device' });
+    accelerometer.addEventListener('error', (event) => {
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+            // Branch to code for requesting permission.
+        } else if (event.error.name === 'NotReadableError' ) {
+            console.log('Cannot connect to the sensor.');
+            error_card.style.display = "block";
+            error_message.innerHTML = "Keine Verbindung zum Sensor möglich.";
+        }
+    });
+    accelerometer.addEventListener('reading', () => reloadOnShake(accelerometer));
+    accelerometer.start();
+} catch (error) {
+    error_card.style.display = "block";
+    // Handle construction errors.
+    if (error.name === 'SecurityError') {
+        // See the note above about permissions policy.
+        console.log('Sensor construction was blocked by a permissions policy.');
+    } else if (error.name === 'ReferenceError') {
+        console.log('Sensor is not supported by the User Agent.');
+        error_message.innerHTML = "Sensor API wird vom Browser nicht unterstüzt.";
+    } else {
+        throw error;
+    }
+  };
+  accelerometer.addEventListener("reading", () => {
+    var ax = accelerometer.x
+    var ay = accelerometer.y
+    var az = accelerometer.z
+  
+    document.querySelector("#x_acc").innerHTML = "X = " + ax;
+    document.querySelector("#y_acc").innerHTML = "Y = " + ay;
+    document.querySelector("#z_acc").innerHTML = "Z = " + az;
+  });
+  accelerometer.start();
