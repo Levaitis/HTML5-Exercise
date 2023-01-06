b9c5d9d96ccd54a4b596956e7ff6cff5b2503521
Leonardo Vasi <leonardo@vasi.de>
2023-01-06 14:52:43 +0100

    Feature: Added error handling if no acceleration sensor is avaiable.

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -1,15 +1,41 @@
-window.ondevicemotion = function(event) { 
-	var ax = event.accelerationIncludingGravity.x
-	var ay = event.accelerationIncludingGravity.y
-	var az = event.accelerationIncludingGravity.z
+let accelerometer = null;
+var error_card = document.getElementById("error_acc");
+var error_message = document.getElementById("error_acc_msg");
+error_card.style.display = "none";
+try {
+    accelerometer = new Accelerometer({ referenceFrame: 'device' });
+    accelerometer.addEventListener('error', (event) => {
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+            // Branch to code for requesting permission.
+        } else if (event.error.name === 'NotReadableError' ) {
+            console.log('Cannot connect to the sensor.');
+            error_card.style.display = "block";
+            error_message.innerHTML = "Keine Verbindung zum Sensor möglich.";
+        }
+    });
+    accelerometer.addEventListener('reading', () => reloadOnShake(accelerometer));
+    accelerometer.start();
+} catch (error) {
+    error_card.style.display = "block";
+    // Handle construction errors.
+    if (error.name === 'SecurityError') {
+        // See the note above about permissions policy.
+        console.log('Sensor construction was blocked by a permissions policy.');
+    } else if (error.name === 'ReferenceError') {
+        console.log('Sensor is not supported by the User Agent.');
+        error_message.innerHTML = "Sensor wird vom Browser nicht unterstüzt.";
+    } else {
+        throw error;
+    }
+  };
+  accelerometer.addEventListener("reading", () => reloadOnShake(accelerometer));
+  accelerometer.start();
+  
+  var ax = accelerometer.x
+  var ay = accelerometer.y
+  var az = accelerometer.z
 
-	document.querySelector("#x_acc").innerHTML = "X = " + ax;
-	document.querySelector("#y_acc").innerHTML = "Y = " + ay;
-	document.querySelector("#z_acc").innerHTML = "Z = " + az;
-}
-
-window.addEventListener("deviceorientation", function(event) {
-	document.querySelector("#mag_alpha").innerHTML = "alpha = " + event.alpha;
-	document.querySelector("#mag_beta").innerHTML = "beta = " + event.beta;
-	document.querySelector("#mag_gamma").innerHTML = "gamma = " + event.gamma;
-}, true);
+  document.querySelector("#x_acc").innerHTML = "X = " + ax;
+  document.querySelector("#y_acc").innerHTML = "Y = " + ay;
+  document.querySelector("#z_acc").innerHTML = "Z = " + az;
